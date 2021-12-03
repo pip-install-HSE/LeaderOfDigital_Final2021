616a110d2a7be69a541401ef6fb911aab4d5477c
6a16ec <85ababobkziQVMew6a16ec>
2021-12-03 05:09:29 +0300

    Fast API new version

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-import json
 from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
 
@@ -27,34 +27,49 @@ def read_item(item_id: int):
     return {"item_id": item_id}
 
 
-def huy(name, geometry):
-    return {
-        "id": 1,
-        "geometry": geometry,
-        "name": name,
-        "tags": {"Организация": "ООО Газпром"},
-        "padding": 10
-    }
+# def huy(name, geometry):
+#     return {
+#         "id": 1,
+#         "geometry": geometry,
+#         "name": name,
+#         "tags": {"Организация": "ООО Газпром"},
+#         "padding": 10
+#     }
 
 
 @app.get("/search/")
 def read_item(object_name: str):
-    object_name = object_name.lower()
     # objects = ['Завод имени Чкалова', 'Завод имени Петрова', 'Трубопровод Петечкина']
     with open('us-states.geojson.txt', 'r') as file:
         geojson = json.loads(file.read())
     features = geojson['features']
     objects = [(feature['properties']['name'], i) for i, feature in enumerate(features)]
-    objects = [(name.lower(), i) for (name, i) in objects]
     result = []
+    filtered_geojson = geojson
+    filtered_geojson['features'] = []
     for name, i in objects:
-        if name in object_name or object_name in name:
+        if name.lower() in object_name.lower() or object_name.lower() in name.lower():
+            filtered_geojson['features'].append(features[i])
             result.append({
-                "id": 1,
-                "geometry": features[i]['geometry'],
+                "id": i,
+                # "geometry": features[i]['geometry'],
                 "name": name,
                 "tags": {"Организация": "ООО Газпром"},
                 "padding": 10
             })
+
     print(len(result))
-    return result
+    return result, filtered_geojson
+
+
+@app.get("/get_object/")
+def read_item(object_id: int):
+    with open('us-states.geojson.txt', 'r') as file:
+        geojson = json.loads(file.read())
+    return geojson['features'][object_id]
+
+
+@app.get("/objects_near/")
+def read_item(polygon: str):
+    # делаем запрос к бд, возвращаем рядом которые с учетом паддингов
+    return None
